fd6d65c2f8dfb6325dff9414eb5ae5dc7a9b5b9f
Offset-X <72325118+Offset-X@users.noreply.github.com>
2020-10-03 19:42:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<html><head><title>Kukulcan Team n_n</title></head>
+<body>
+
+<br>
+<link href='http://fonts.googleapis.com/css?family=Orbitron:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Josefin Sans' rel='stylesheet' type='text/css'>
+<body bgcolor="#000000" background =><div class='CenterDiv'>
+<center>
+<h1><center><font color="red" face=Orbitron>HACKED BY KUKULCAN TEAM n_n<h1></font>
+<img src=https://i0.wp.com/kukulkanmxcom.wpcomstaging.com/wp-content/uploads/2018/08/centered_portada_negro.jpg?w=1250&ssl=1 width=450px height=340px>
+
+
+<body onload="init()"></body>
+<body>
+
+<div id="bulle"></div>
+<script language="JavaScript">
+var i=0
+var j=0
+var texteNE, affiche
+var texte="<br><br><br><br><br><font face=Orbitron color=green size=4>Administrador Tu Seguridad es una Burla <br> Esto Sucede cuando  no se contrata personal capacitado <br><br>  Empezaremos a publicar información si no vemos un cambio, Estamos hartos de la corrupción en la Coordinación de Informática , Departamento de Matemáticas <br>  <br> USTEDES SIEMPRE SIRVIENDO A INTERESES PERSONALES Y NO AL DE LOS ALUMNOS <br><br> ESTO NO ES UN ADIOS <br> NOS VEMOS PRONTO  <br><br> HACKED BY KUKULCAN TEAM n_n<br><br><br></font><br></b></div>"
+var ie = (document.all);
+var ne = (document.layers); 
+function init(){
+texteNE='';
+machine_a_ecrire();
+}
+function machine_a_ecrire(){
+texteNE=texteNE+texte.charAt(i)
+affiche='<font face=Orbitron size=1 color=#ffffff><strong>'+texteNE+'</strong></font>'
+if (texte.charAt(i)=="<") {
+j=1
+}
+if (texte.charAt(i)==">") {
+j=0
+}
+if (j==0) {
+if (document.getElementById) { // avec internet explorer
+document.getElementById("bulle").innerHTML = affiche;
+}
+}
+if (i<texte.length-1){
+i++
+setTimeout("machine_a_ecrire()",70)
+}
+else
+return
+}
+</script><font face="Orbitron" size="1"><blink><span style="color: rgb(255, 255, 255);"><b><font color=lime size=4></font></b></u></blink><br></font></b>
+<a href="http://veneno.ovh"></a></body>
+
+<!-- CSS --><style>
+.CenterDiv{width:650px;border:1px #ff0000 solid;padding:5px;margin:0px auto; background: url('');}
+</style>
+
+
+<br>
+<br>
+<br>
+
+<audio controls autoplay>
+    <source src="https://www.mboxdrive.com/Tenemos%20dificultades%20te%CC%81cnicas..mp3" type="audio/mp3">
+       
+</audio>
+
